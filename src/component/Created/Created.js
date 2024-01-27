b3585331e5c0dcc82eb23dbbf8712dbf2eff6ae2
Saurav <sauravkumar00108@gmail.com>
2024-01-27 20:26:32 +0530

    all changes

--- src/component/Created/Created.js
+++ src/component/Created/Created.js
@@ -1,27 +1,134 @@
-import React from 'react'
-import "./Created.css"
+import React from "react";
+import "./Created.css";
 
 function Created() {
   return (
-    <div className='section_created_on'>
-      <div className='relative'>
-        <div className='created_on_shadows_top w-embed'></div>
+    <div className="section_created_on">
+      <div className="relative">
+        <div className="created_on_shadows_top w-embed"></div>
       </div>
-      <div className='created_on_wrapper'>
-        <div className='w-layout-blockcontainer heygen_container created-on w-container'>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+      <div className="created_on_wrapper">
+        <div className="w-layout-blockcontainer heygen_container created-on w-container">
+        <div
+  className="created_on_item_1"
+  style={{
+    willChange: 'transform',
+    transform: 'translate3d(0px, -8.5638vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
+    transformStyle: 'preserve-3d'
+  }}
+>            <img
+              src="https://assets-global.website-files.com/63fdcbe48aa020c0a5493507/656784f4a1a5876ea1668c22_creator-2.webp"
+              className="i1"
+              sizes="(max-width: 767px) 177px, (max-width: 991px) 280px, 388px"
+              alt=""
+            />
+          </div>
+          <div
+            className="created_on_item_2"
+            style={{
+              willChange: "transform",
+              transform:
+                "translate3d(0px, -10.3325vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+              transformStyle: "preserve-3d",
+            }}
+          >
+            <img
+              src="https://assets-global.website-files.com/63fdcbe48aa020c0a5493507/6567286fa483986cef4ac893_creator-1.webp"
+              alt=""
+            />
+          </div>
+          <div
+            className="created_on_item_4"
+            style={{
+              willChange: "transform",
+              transform:
+                "translate3d(0px, 6.48568vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+              transformStyle: "preserve-3d",
+            }}
+          >
+            {" "}
+            <img
+              src="https://assets-global.website-files.com/63fdcbe48aa020c0a5493507/6567286e5148720d52b54287_iPhone%2014%20-%2023.webp"
+              alt=""
+            />
+          </div>
+          <div
+            className="created_on_item_5"
+            style={{
+              willChange: "transform",
+              transform:
+                "translate3d(0px, 0vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+              transformStyle: "preserve-3d",
+            }}
+          >
+            {" "}
+            <img
+              src="https://assets-global.website-files.com/63fdcbe48aa020c0a5493507/656784f2ca5a52c0df8c8b02_creator-3.webp"
+              alt=""
+            />
+          </div>
+          <div
+            className="created_on_item_6"
+            style={{
+              willChange: "transform",
+              transform:
+                "translate3d(0px, 3.77204vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+              transformStyle: "preserve-3d",
+            }}
+          >
+            {" "}
+            <img
+              src="https://assets-global.website-files.com/63fdcbe48aa020c0a5493507/6567286e6cab2a3cb6cdcfc6_creator-2.webp"
+              alt=""
+            />
+          </div>
+          <div
+            className="created_on_item_7"
+            style={{
+              willChange: "transform",
+              transform:
+                "translate3d(0px, 0vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+              transformStyle: "preserve-3d",
+            }}
+          >
+            {" "}
+            <img
+              src="https://assets-global.website-files.com/63fdcbe48aa020c0a5493507/656784f4ba607c947979e46f_creator-1.webp"
+              alt=""
+            />
+          </div>
+          <div
+            className="created_on_item_8"
+            style={{
+              willChange: "transform",
+              transform:
+                "translate3d(0px, 5vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+              transformStyle: "preserve-3d",
+            }}
+          >
+            {" "}
+            <img
+              src="https://assets-global.website-files.com/63fdcbe48aa020c0a5493507/6567286eff8b439e0d8230bf_iPhone%2014%20-%2024.webp"
+              alt=""
+            />
+          </div>
         </div>
-        <div className='created_on_text_wrapper'></div>
+        <div className="created_on_text_wrapper">
+          <div>
+            <h2 className="heygen_section_heading_feature is-createdon">
+              <span className="text-span-59">
+                Created with<br></br>
+              </span>
+              <span className="purple-span">HeyGen</span>
+            </h2>
+          </div>
+        </div>
       </div>
-      <div className='created_on_bottom'></div>
+      <div className="created_on_bottom">
+        <div className="created_on_shadows_bottom w-embed"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Created
+export default Created;
